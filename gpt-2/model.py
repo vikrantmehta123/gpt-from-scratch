6bf37ed8a26666d4f7209dfb440337c60df2cbc5
Vikrant Mehta <vikrantmehta123@gmail.com>
2024-12-19 14:40:50 +0530

    initial commit

---
+++ gpt-2/model.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embd: int = 384
+
+class MLP(nn.Module):
+    pass
+
+class CausalSelfAttention(nn.Module):
+    pass 
+
+class Block(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = nn.CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class GPT(nn.Module):
+
+    def __init__(self, config:GPTConfig):
+        super().__init__()
+        self.config = config
+        
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd), # text embeddings
+            wpe = nn.Embedding(config.block_size, config.n_embd), # positional embeddings
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]), 
+            ln_final = nn.LayerNorm(config.n_embd)
+        )) 
+
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
